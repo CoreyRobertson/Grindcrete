e01213663b63bee12375fd68232cb241a5fbed14
coreyr <coreystephenr@gmail.com>
2024-05-31 12:08:34 +1000

    google tag commit

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,7 @@ import Footer from '@/components/Footer' // Adjust the path as necessary
 import { Analytics } from "@vercel/analytics/react"
 import { useEffect } from 'react';
 import { useRouter } from 'next/router';
+import Head from 'next/head';
 import * as gtag from '../lib/gtag'; // Adjust the path as necessary
 
 export default function App({ Component, pageProps }) {
@@ -22,6 +23,17 @@ export default function App({ Component, pageProps }) {
 
   return (
     <>
+      <Head>
+        {/* Google Analytics Tag */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-ES8GXE4CXC"></script>
+        <script dangerouslySetInnerHTML={{
+          __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-ES8GXE4CXC');
+        `}} />
+      </Head>
       <Navbar />
       <main>
         <Component {...pageProps} />
