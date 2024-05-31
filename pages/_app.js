13a1dd266b5b63a168df8fb62d6b9dd11facd6e3
coreyr <coreystephenr@gmail.com>
2024-05-31 11:42:53 +1000

    google analytics commit

--- pages/_app.js
+++ pages/_app.js
@@ -2,8 +2,24 @@ import '@/styles/globals.css'
 import Navbar from '@/components/Navbar' // Adjust the path as necessary
 import Footer from '@/components/Footer' // Adjust the path as necessary
 import { Analytics } from "@vercel/analytics/react"
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import * as gtag from '../lib/gtag'; // Adjust the path as necessary
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <>
       <Navbar />
@@ -13,5 +29,5 @@ export default function App({ Component, pageProps }) {
       </main>
       <Footer />
     </>
-  )
+  );
 }
